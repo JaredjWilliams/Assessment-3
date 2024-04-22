ec9a32431e3f5a31c91d5d420b2257922ba65d43
jsparks9 <josiahsparks78@gmail.com>
2024-04-22 14:30:48 -0500

    ng serve working after redux install

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,6 @@ import { UserRegistryComponent } from './user-registry/user-registry.component';
 import { HeaderComponent } from './header/header.component';
 import { CreateAnnouncementComponent } from './overlays/create-announcement/create-announcement.component';
 import { CreateTeamComponent } from './overlays/create-team/create-team.component';
-import { CreateProjectsComponent } from './overlays/create-projects/create-projects.component';
 import { CreateProjectComponent } from './overlays/create-project/create-project.component';
 import { EditProjectComponent } from './overlays/edit-project/edit-project.component';
 import { AddUserComponent } from './overlays/add-user/add-user.component';
@@ -30,7 +30,6 @@ import { DropDownComponent } from './utility-views/drop-down/drop-down.component
     HeaderComponent,
     CreateAnnouncementComponent,
     CreateTeamComponent,
-    CreateProjectsComponent,
     CreateProjectComponent,
     EditProjectComponent,
     AddUserComponent,
@@ -38,7 +37,8 @@ import { DropDownComponent } from './utility-views/drop-down/drop-down.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]

b27cea893468af7ca36b4dbee9f3c64e08cccffa
Jared Williams <jared.williams101697@gmail.com>
2024-04-23 22:33:58 -0400

    Removed unused code

--- frontend/src/app/teams/teams.component.ts
+++ frontend/src/app/teams/teams.component.ts
@@ -4,7 +4,6 @@ import { AuthState, selectTeams, selectUser } from '../auth/auth.reducer';
 import { Observable } from 'rxjs';
 import Team from '../models/Team';
 import { Router } from '@angular/router';
-import {TeamService} from "../services/team/team.service";
 
 @Component({
   selector: 'app-teams',
@@ -19,26 +18,10 @@ export class TeamsComponent  implements OnInit {
   constructor(
     private store: Store<AuthState>,
     private router: Router,
-    private teamService: TeamService
     ) { }
 
   ngOnInit(): void {
     this.teams$ = this.store.pipe(select(selectTeams));
     console.log(this.teams$);
-  }
-
-  showProjects(team: any): void {
-    this.router.navigate(['/projects', { team: JSON.stringify(team) }]);
-  }
-
-  getTeams(id: string) : void {
-    this.teamService.getTeams("6").subscribe({
-      next: (teams) => {
-        this.teams = teams;
-        console.log(teams);
-      },
-      error: (err) => {
-        console.log(err);
-      }});
   }
 }
